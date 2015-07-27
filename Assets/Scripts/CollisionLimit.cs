5fdc34a93a07d6a9761e1f84f5bbe3f945e47462
Niklas Weber <niklas_weber.1403@gmx.de>
2015-07-27 19:14:59 +0200

    Version 1

---
+++ Assets/Scripts/CollisionLimit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionLimit : MonoBehaviour {
+
+	// Use this for initialization
+	void onTriggerEnter(Collider other) {
+	
+//		if (other.gameObject.tag == "Projectile") {
+			Destroy (other.gameObject);
+//		FollowCamera.S.poi = null;
+//		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
