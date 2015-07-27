9ee90c1f29958ba80411293d8ab269847e3076ca
Niklas Weber <niklas_weber.1403@gmx.de>
2015-07-27 19:18:39 +0200

    Version 3

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+	
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+
+	}
+
+	// Update is called once per frame
+	public void StartLevel () {
+	
+		Application.LoadLevel (1);
+
+	}
+
+	public void ExitGame () {
+
+		Application.Quit ();
+
+	}
+}
